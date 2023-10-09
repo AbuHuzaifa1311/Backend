3f05abb21102f77a4a43c4843272f955894682d9
AbuHuzaifa <70134222@student.uol.edu.pk>
2023-10-09 16:54:03 +0500

    Rename string

--- string.py
+++ string.py
@@ -13,7 +13,23 @@
 # print(price,with_tax)
 # print("Base price: ${:.2f},with tax: ${:.2f}".format(price,with_tax))
 
-def to_celsius(x):
-    return (x-32)*5/9
-for x in range(0,101,10):
-    print("{:>3} F | {:>6.2f} C".format(x,to_celsius(x)))
+# def to_celsius(x):
+#     return (x-32)*5/9
+# for x in range(0,101,10):
+#     print("{:>3} F | {:>6.2f} C".format(x,to_celsius(x)))
+# fruits = ["pineapple", "Banana", "Apple", "Melon"]
+# fruits.insert(0,"orange")
+# print(fruits)
+# fruits = ["pineapple", "Banana", "Apple", "Melon"]
+# fruits.pop(2)
+# 'Apple'
+# print(fruits)
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - houes * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+ 
+  result = convert_seconds (5000)
+ print(result)
+
